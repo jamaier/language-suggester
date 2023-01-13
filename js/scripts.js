6a5b90a2cc3622eaed7b3906ba27ed4b11e9d068
Jacob Maier <jacobamaier@gmail.com>
2023-01-13 09:58:58 -0800

    Update Html submit and result tab. Also, Define variables and add .onload funtion.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+  function preventDefault(event) {
+    event.preventDefault();
+  }
+
+  const questionOne = document.getElementsByClassName("questionOne");
+  const questionTwo = document.getElementsByClassName("questionTwo");
+  const questionThree = document.getElementsByClassName("questionThree");
+  const questionFour = document.getElementsByClassName("questionFour");
+  const questionFive = document.getElementsByClassName("questionFive");
+  const submit = document.getElementById("submit");
+  const result = document.getElementsByClassName("result");
+}
