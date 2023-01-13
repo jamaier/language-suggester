5e72390073fd6aaa05e9424ed2e3ab5c3346f79b
Jacob Maier <jacobamaier@gmail.com>
2023-01-13 13:40:07 -0800

    WIP: Test first radio button but Bug showing up for const.

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,87 @@
-function hideResults() {
-  document.getElementById("python").setAttribute("class", "hidden");
-  document.getElementById("javascript").setAttribute("class", "hidden");
-  document.getElementById("swift").setAttribute("class", "hidden");
-  document.getElementById("reason").setAttribute("class", "hidden");
-  document.getElementById("rather").setAttribute("class", "hidden");
+window.onload = function() {
+
+const form = document.getElementsByClassName("questionOne"); //, "questionTwo", "questionThree", "questionFour", "questionFive")
+  const resultDivs = {  
+  python: document.getElementById("python"),
+  javascript: document.getElementById("javascript"),
+  swift: document.getElementById("swift"),
+  reasonWhy: document.getElementById("reasonWhy"),
+  rather: document.getElementById("rather"),
+  };
+
+  form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const bigData = document.getElementById("data").checked === true;
+  const websites = document.getElementById("websites").checked === true;
+
+  if (bigData) {
+    resultDivs.python.classList.remove("hidden");
+  } else (websites) {
+    resultDivs.javascript.classList.remove("hidden");
+  }
+})
 }
 
 
-const questionOne = document.getElementById("questionOne");
-// const questionTwo = document.getElementById("questionTwo");
-// const questionThree = document.getElementById("questionThree");
-// const questionFour = document.getElementById("questionFour");
-// const questionFive = document.getElementById("questionFive");
 
 
-window.onload = function () {
-  function preventDefault(event) {
-    event.preventDefault();
-    hideResults();
+// function hideResults() {
+//   document.getElementById("python").setAttribute("class", "hidden");
+//   document.getElementById("javascript").setAttribute("class", "hidden");
+//   document.getElementById("swift").setAttribute("class", "hidden");
+//   document.getElementById("reason").setAttribute("class", "hidden");
+//   document.getElementById("rather").setAttribute("class", "hidden");
+// }
 
-    const form = document.querySelector("form");
+// const questionOne = document.getElementById("questionOne");
+// // const questionTwo = document.getElementById("questionTwo");
+// // const questionThree = document.getElementById("questionThree");
+// // const questionFour = document.getElementById("questionFour");
+// // const questionFive = document.getElementById("questionFive");
 
-    questionOne.addEventListener("change", suggestLanguage);
-    // questionTwo.addEventListener("change", suggestLanguage);
-    // questionThree.addEventListener("change", suggestLanguage);
-    // questionFour.addEventListener("change", suggestLanguage);
-    // questionFive.addEventListener("change", suggestLanguage);
-    form.addEventListener("submit", preventDefault);
+// window.onload = function () {
+//   function preventDefault(event) {
+//     event.preventDefault();
+//     hideResults();
 
-    function suggestLanguage() {
-      let language = "";
+//     const form = document.querySelector("form");
 
-      if (questionOne.value === "Mobile") {
-        if (questionTwo.value === "data") {
-          language = "Python";
-        } else if (questionTwo.value === "games") {
-          language = "JavaScript";
-        } else {
-          language = "Swift";
-        }
-      } else if (questionOne.value === "Desktop") {
-        if (questionTwo.value === "data") {
-          language = "Python";
-        } else if (questionTwo.value === "games") {
-          language = "JavaScript";
-        } else {
-          language = "Swift";
-        }
-      } else {
-        if (questionTwo.value === "data") {
-          language = "JavaScript";
-        } else if (questionTwo.value === "games") {
-          language = "JavaScript";
-        } else {
-          language = "Python";
-        }
-      };
-    };
-  };
-};
+//     questionOne.addEventListener("change", suggestLanguage);
+//     // questionTwo.addEventListener("change", suggestLanguage);
+//     // questionThree.addEventListener("change", suggestLanguage);
+//     // questionFour.addEventListener("change", suggestLanguage);
+//     // questionFive.addEventListener("change", suggestLanguage);
+//     form.addEventListener("submit", preventDefault);
+
+//     function suggestLanguage() {
+//       let language = "";
+
+//       if (questionOne.value === "Mobile") {
+//         if (questionTwo.value === "data") {
+//           language = "Python";
+//         } else if (questionTwo.value === "games") {
+//           language = "JavaScript";
+//         } else {
+//           language = "Swift";
+//         }
+//       } else if (questionOne.value === "Desktop") {
+//         if (questionTwo.value === "data") {
+//           language = "Python";
+//         } else if (questionTwo.value === "games") {
+//           language = "JavaScript";
+//         } else {
+//           language = "Swift";
+//         }
+//       } else {
+//         if (questionTwo.value === "data") {
+//           language = "JavaScript";
+//         } else if (questionTwo.value === "games") {
+//           language = "JavaScript";
+//         } else {
+//           language = "Python";
+//         }
+//       };
+//     };
+//   };
+// };
