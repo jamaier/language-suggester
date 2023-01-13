07c14538b261649389f9011b84da64455c8cb82c
Jacob Maier <jacobamaier@gmail.com>
2023-01-13 15:24:44 -0800

    WIP: more logic issues with driver of computer.

--- scripts.js
+++ scripts.js
@@ -1,20 +1,16 @@
-
-
 const bigData = document.querySelector('input[name="bigData"]:checked') === "true";
 const buildWebsites = document.querySelector('input[name="buildWebsites"]:checked') === "true";
 
   window.onload = function () {
-  document.querySelector('.formOne').addEventListener('submit',  function(event) {
+  document.querySelector('.formOne').addEventListener('submit', function(event) {
     event.preventDefault();
-    addEventListener('reset', (event) => {});
   })
 
-  
 
     if (bigData === "bigData") {
         document.getElementById("python").classList.remove("hidden");
     } else {
       (buildWebsites === "buildWebsites") 
-        document.getElementById("swift").classList.remove("hidden");
+        document.getElementById("javascript").classList.remove("hidden");
       }
     };
