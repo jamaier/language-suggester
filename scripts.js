f5ae6817af8400068af05d8b2fe60e52deecbbd4
Jacob Maier <jacobamaier@gmail.com>
2023-01-13 16:23:47 -0800

    Insert basic logic and branching. Change for from class to id.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,36 @@
-const bigData = document.querySelector('input[name="bigData"]:checked') === "true";
-const buildWebsites = document.querySelector('input[name="buildWebsites"]:checked') === "true";
+function languageSuggestion() {
 
-  window.onload = function () {
-  document.querySelector('.formOne').addEventListener('submit', function(event) {
-    event.preventDefault();
-  })
 
-    if (bigData === "bigData") {
-        document.getElementById("python").classList.remove("hidden");
-    } else {
-      (buildWebsites === "buildWebsites") 
-        document.getElementById("javascript").classList.remove("hidden");
-      }
-    };
+  const questionOne = document.querySelector("input[name='questionOne']:checked")
+  const questionTwo = document.querySelector("input[name='questionTwo']:checked")
+  const questionThree = document.querySelector("input[name='questionThree']:checked")
+  const questionFour = document.querySelector("input[name='questionFour']:checked")
+  const questionFive = document.querySelector("input[name='questionFive']:checked")
+
+  if (questionOne === "data" && questionTwo === "desktop") {
+    document.querySelector("div#python").removeAttribute("hidden")
+  } else if (questionThree === "home") {
+    document.querySelector("div#javascript").removeAttribute("hidden")
+  } else {
+    document.querySelector("div#swift").removeAttribute("hidden")
+  }
+
+  if (questionFour === "career") {
+    document.querySelector("div#reasonWhy").removeAttribute("hidden")
+  } else {
+    document.querySelector("div#reasonWhy").removeAttribute("hidden")
+  }
+
+  if (questionFive === "rather") {
+    document.querySelector("div#rather").removeAttribute("hidden")
+  } else {
+    document.querySelector("div#rather").removeAttribute("hidden")
+  }
+};
+
+window.addEventListener("load", function(event) {
+  const form = document.getElementById("formOne");
+  form.addEventListener("submit", languageSuggestion)
+  event.preventDefault();
+  languageSuggestion();
+});
