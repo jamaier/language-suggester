2a0c9370d1a464b4f7610f0fed2107bd530c1170
Jacob Maier <jacobamaier@gmail.com>
2023-01-15 21:24:55 -0800

    BUGFIX: Found codebreaking bug in the submit button being improperly labeled.

--- js/scripts.js
+++ js/scripts.js
@@ -6,84 +6,27 @@ function hideResults() {
   document.getElementById("rather").setAttribute("class", "hidden");
 }
 
-
-window.addEventListener("load", function () {
-  document.getElementById("radioForm").addEventListener("submit", languageSuggestion);
-  hideResults();
-  function languageSuggestion (event) {
+window.addEventListener("load", function() {
+  let form = document.getElementById("userForm");
+  form.addEventListener("submit", (event) => {
     event.preventDefault();
+    hideResults();
 
-  const questionOne = document.querySelector("input[name='questionOne']:checked").value;
-  const questionTwo = document.querySelector("input[name='questionTwo']:checked").value;
-  const questionThree = document.querySelector("input[name='questionThree']:checked").value;
-  const questionFour = document.querySelector("input[name='questionFour']:checked").value;
-  const questionFive = document.querySelector("input[name='questionFive']:checked").value;
+    const questionOne = parseInt(document.getElementById("questionOne").value);
+    const questionTwo = parseInt(document.getElementById("questionTwo").value);
+    const questionThree = parseInt(document.getElementById("questionThree").value);
+    const questionFour = parseInt(document.getElementById("questionFour").value);
+    const questionFive = parseInt(document.getElementById("questionFive").value);
 
-  if (questionOne === "data" && questionTwo === "desktop" && questionThree === "home") {
-    document.getElementById("python").removeAttribute("class");
-  } else if (questionTwo === "mobile" && questionThree === "office") {
-    document.getElementById("swift").removeAttribute("class");
-  } else {
-      document.getElementById("rather").removeAttribute("class")
-  }
-  // if (questionFour === "career" && questionFive === "stubToe") {
-  //   document.getElementById("reasonWhy").removeAttribute("class");
-  // } else if (userName && bday && animal === 4 || family === 4) {
-  //   document.getElementById("csharp").removeAttribute("class");
-  // } else if (userName && bday) {
-  //   document.getElementById("ruby").removeAttribute("class");
-  // } else {
-  //   document.getElementById("error-message").removeAttribute("class");
-  // }
-  }
-});
+    const results = questionOne + questionTwo + questionThree + questionFour + questionFive
 
+      if (results <=5 ) {
+        document.getElementById("python").removeAttribute("class", "hidden");
+      } else if (results >= 6) {
+        document.getElementById("javascript").removeAttribute("class", "hidden");
+      } else {
+        
+      }
+    });
+  });
 
-
-// function hideResults() {
-//   document.getElementById("python").setAttribute("class", "hidden");
-//   document.getElementById("javascript").setAttribute("hidden");
-//   document.getElementById("swift").setAttribute("hidden");
-//   document.getElementById("readonWhy").setAttribute("hidden");
-//   document.getElementById("rather").setAttribute("hidden");
-// }
-// function languageSuggestion () {
-// const questionOne = document.querySelector("input[name='questionOne']:checked");
-// const questionTwo = document.querySelector("input[name='questionTwo']:checked");
-// const questionThree = document.querySelector(
-//   "input[name='questionThree']:checked"
-// );
-// const questionFour = document.querySelector(
-//   "input[name='questionFour']:checked"
-// );
-// const questionFive = document.querySelector(
-//   "input[name='questionFive']:checked"
-// );
-
-
-
-// if (questionOne === "data" && questionTwo === "desktop") {
-//   document.querySelector("div#python").removeAttribute("class");
-// } else if (questionThree === "home") {
-//   document.querySelector("div#javascript").removeAttribute("class");
-// } else {
-//   document.querySelector("div#swift").removeAttribute("class");
-// };
-
-// if (questionFour === "career" && questionFour === "forFun") {
-//   document.querySelector("div#reasonWhy").removeAttribute("class");
-// } else {
-// };
-
-// if (questionFive === "bear" && questionFive === "stubToe") {
-//   document.querySelector("div#rather").removeAttribute("class");
-// } else {
-// };
-// }
-
-// window.addEventListener("load", function (event) {
-//   const form = document.getElementById("formOne");
-//   form.addEventListener("submit", languageSuggestion);
-//   event.preventDefault();
-//   hideResults();
-// });
